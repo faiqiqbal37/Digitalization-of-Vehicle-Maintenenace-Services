af344bddd1b2cb3589806ee5556c3675d94eed63
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-07-27 02:04:08 +0100

    created the model for the customer

---
+++ lib/models/customer/customer.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'customer.freezed.dart';
+part 'customer.g.dart';
+
+@freezed
+class Customer with _$Customer {
+  const factory Customer({
+    required int id,
+    required String firstname,
+    required String lastname,
+    required String phoneNumber, // Now required
+    required String email,
+    required String password,
+    String? location, // Optional
+  }) = _Customer;
+
+  factory Customer.fromJson(Map<String, dynamic> json) => _$CustomerFromJson(json);
+}
---
+++ lib/models/customer/customer.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'customer.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CustomerImpl _$$CustomerImplFromJson(Map<String, dynamic> json) =>
+    _$CustomerImpl(
+      id: (json['id'] as num).toInt(),
+      firstname: json['firstname'] as String,
+      lastname: json['lastname'] as String,
+      phoneNumber: json['phoneNumber'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      location: json['location'] as String?,
+    );
+
+Map<String, dynamic> _$$CustomerImplToJson(_$CustomerImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'firstname': instance.firstname,
+      'lastname': instance.lastname,
+      'phoneNumber': instance.phoneNumber,
+      'email': instance.email,
+      'password': instance.password,
+      'location': instance.location,
+    };
