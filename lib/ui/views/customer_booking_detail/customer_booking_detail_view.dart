5c42f18d977ce79ea99946fdf1a0c82f528ae60d
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-08-07 20:26:19 +0100

    refined the UI for customer_bookings_detail screen

--- lib/ui/views/customer_booking_detail/customer_booking_detail_view.dart
+++ lib/ui/views/customer_booking_detail/customer_booking_detail_view.dart
@@ -4,8 +4,7 @@ import 'package:stacked/stacked.dart';
 
 import 'customer_booking_detail_viewmodel.dart';
 
-class CustomerBookingDetailView
-    extends StackedView<CustomerBookingDetailViewModel> {
+class CustomerBookingDetailView extends StackedView<CustomerBookingDetailViewModel> {
   final String serviceName;
   final String serviceProviderName;
   final String price;
@@ -16,90 +15,117 @@ class CustomerBookingDetailView
 
   const CustomerBookingDetailView(
       {Key? key,
-      required this.phone,
-      required this.email,
-      required this.price,
-      required this.date,
-      required this.serviceName,
-      required this.serviceProviderName,
-      required this.status})
+        required this.phone,
+        required this.email,
+        required this.price,
+        required this.date,
+        required this.serviceName,
+        required this.serviceProviderName,
+        required this.status})
       : super(key: key);
 
   @override
   Widget builder(
-    BuildContext context,
-    CustomerBookingDetailViewModel viewModel,
-    Widget? child,
-  ) {
+      BuildContext context,
+      CustomerBookingDetailViewModel viewModel,
+      Widget? child,
+      ) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 60),
-          Icon(Icons.calendar_month, size: 70, color: Colors.black),
-          Text("Booking Detail",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-          SizedBox(height: 20),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Service Information:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)),
-                    ListTile(
-                      leading: Icon(Icons.build),
-                      title: Text(serviceName),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.garage),
-                      title: Text(serviceProviderName),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.monetization_on),
-                      title: Text(price),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text(phone),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.email),
-                      title: Text(email),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.location_on),
-                      title: Text("null"),
-                    ),
-                    SizedBox(height: 20),
-                    Text('Booking Details:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16)),
-                    ListTile(
-                      leading: Icon(Icons.date_range),
-                      title: Text(date),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.check),
-                      title: Text(status),
-                    ),
-                    SizedBox(height: 20),
-                  ],
+      appBar: AppBar(
+        title: Text('Booking Detail'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Icon(Icons.calendar_month, size: 110, color: Colors.black),
+              ),
+              SizedBox(height: 20),
+              Card(
+                color: Colors.white,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Service Information',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey),
+                      ),
+                      SizedBox(height: 20),
+                      ListTile(
+                        leading: Icon(Icons.build),
+                        title: Text(serviceName, style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.garage),
+                        title: Text(serviceProviderName, style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.monetization_on),
+                        title: Text('£$price', style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text(phone, style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.email),
+                        title: Text(email, style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.location_on),
+                        title: Text("null", style: TextStyle(fontSize: 15)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 20),
+              Card(
+                color: Colors.white,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Booking Details',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey),
+                      ),
+                      SizedBox(height: 20),
+                      ListTile(
+                        leading: Icon(Icons.date_range),
+                        title: Text(date, style: TextStyle(fontSize: 15)),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.check),
+                        title: Text(status, style: TextStyle(fontSize: 15)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
       bottomNavigationBar: CustomerBottomNavigationBar(),
     );
   }
 
   @override
-  CustomerBookingDetailViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      CustomerBookingDetailViewModel();
+  CustomerBookingDetailViewModel viewModelBuilder(BuildContext context) => CustomerBookingDetailViewModel();
 }
