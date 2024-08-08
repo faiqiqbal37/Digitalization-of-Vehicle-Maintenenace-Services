dff8209c663d59c11829b95494acdb3163db37cd
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-08-08 18:04:15 +0100

    done with refining the UI of the service provider service listing screen

--- lib/ui/views/service_provider_services/service_provider_services_view.dart
+++ lib/ui/views/service_provider_services/service_provider_services_view.dart
@@ -10,73 +10,90 @@ class ServiceProviderServicesView
 
   @override
   Widget builder(
-    BuildContext context,
-    ServiceProviderServicesViewModel viewModel,
-    Widget? child,
-  ) {
+      BuildContext context,
+      ServiceProviderServicesViewModel viewModel,
+      Widget? child,
+      ) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 60),
-          Icon(Icons.car_repair, size: 80, color: Colors.black),
-          Text("Services",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-          SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: viewModel.servicesNew.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    SizedBox(height: 8),
-                    Card(
+      appBar: AppBar(
+        title: Text('Services'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Icon(Icons.car_repair, size: 80, color: Colors.black),
+            SizedBox(height: 10),
+            Text(
+              "Services",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: viewModel.servicesNew.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       child: ListTile(
-                        leading: Icon(Icons.build),
-                        title: Text(viewModel.servicesNew[index].serviceName),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        leading: Icon(Icons.build, color: Colors.black),
+                        title: Text(
+                          viewModel.servicesNew[index].serviceName,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
                         subtitle: Text(
-                            '${viewModel.servicesNew[index].serviceType} - ${viewModel.servicesNew[index].price}'),
+                          '${viewModel.servicesNew[index].serviceType} - Price: \£${viewModel.servicesNew[index].price}',
+                          style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                        ),
                         trailing: Row(
-                          mainAxisSize: MainAxisSize
-                              .min, // This ensures the Row only takes needed space
+                          mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
                             ElevatedButton(
                               onPressed: () =>
-                                  viewModel.navigateToServiceDetail(
-                                      viewModel.servicesNew[index].id),
+                                  viewModel.navigateToServiceDetail(viewModel.servicesNew[index].id),
                               child: Text('View'),
                               style: ElevatedButton.styleFrom(
                                 foregroundColor: Colors.white,
-                                backgroundColor: Colors
-                                    .blue, // Different color for differentiation
+                                backgroundColor: Colors.blue,
                               ),
                             ),
-                            SizedBox(width: 8), // Spacing between buttons
+                            SizedBox(width: 8),
                             ElevatedButton(
-                              onPressed: () => viewModel.navigateToEditDetail(
-                                  viewModel.servicesNew[index].id),
+                              onPressed: () =>
+                                  viewModel.navigateToEditDetail(viewModel.servicesNew[index].id),
                               child: Text('Edit'),
                               style: ElevatedButton.styleFrom(
                                 foregroundColor: Colors.white,
-                                backgroundColor: Colors.blue, // Text color
+                                backgroundColor: Colors.blue,
                               ),
                             ),
                           ],
                         ),
                       ),
-                    )
-                  ],
-                );
-              },
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           viewModel.navigateToAddService();
         },
+        foregroundColor: Colors.white,
         child: Icon(Icons.add),
         tooltip: 'Add a New Service',
+        backgroundColor: Colors.blue,
       ),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
@@ -84,14 +101,13 @@ class ServiceProviderServicesView
 
   @override
   void onViewModelReady(ServiceProviderServicesViewModel viewModel) {
-    // TODO: implement onViewModelReady
     super.onViewModelReady(viewModel);
     viewModel.fetchServices();
   }
 
   @override
   ServiceProviderServicesViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
+      BuildContext context,
+      ) =>
       ServiceProviderServicesViewModel();
 }
