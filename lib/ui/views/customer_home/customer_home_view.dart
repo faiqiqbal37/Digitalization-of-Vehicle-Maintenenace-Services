e929dcfc37064437ca34cca2bac3ea8d4b2e6612
faiqiqbal37 <55352092+faiqiqbal37@users.noreply.github.com>
2024-08-10 20:32:05 +0100

    Updated the customer home screen

--- lib/ui/views/customer_home/customer_home_view.dart
+++ lib/ui/views/customer_home/customer_home_view.dart
@@ -1,7 +1,7 @@
-import 'package:disertation/ui/views/common_components/customer/navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+import '../common_components/customer/navigation_bar.dart';
 import '../customer_bookings/booking_card.dart';
 import 'booking_widget.dart';
 import 'category_widget.dart';
@@ -12,10 +12,10 @@ class CustomerHomeView extends StackedView<CustomerHomeViewModel> {
 
   @override
   Widget builder(
-    BuildContext context,
-    CustomerHomeViewModel viewModel,
-    Widget? child,
-  ) {
+      BuildContext context,
+      CustomerHomeViewModel viewModel,
+      Widget? child,
+      ) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -70,36 +70,39 @@ class CustomerHomeView extends StackedView<CustomerHomeViewModel> {
               ),
             ),
             SizedBox(height: 30),
-            Text('Upcoming Booking',
+            Text('Upcoming Bookings',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             Container(
               height: 250,
               child: FutureBuilder(
-                  future: viewModel.loadCustomerBookings(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done &&
-                        snapshot.hasData)
-                      return ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            var booking = snapshot.data![index];
-                            return BookingCard(
-                              serviceProviderName:
-                                  booking['serviceProviderName'],
-                              serviceName: booking['serviceName'],
-                              date: booking['date'],
-                              status: booking['status'],
-                              email: booking['email'],
-                              phone: booking['phone'],
-                              price: booking['price'],
-                            );
-                          });
-                    else if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      return CircularProgressIndicator();
+                future: viewModel.loadCustomerBookings(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    var bookings = snapshot.data!.where((booking) => booking['status'] == 'pending').toList();
+                    if (bookings.isEmpty) {
+                      return Center(child: Text('No pending bookings'));
                     }
-                  }),
+                    return ListView.builder(
+                        itemCount: bookings.length,
+                        itemBuilder: (context, index) {
+                          var booking = bookings[index];
+                          return BookingCard(
+                            serviceProviderName: booking['serviceProviderName'],
+                            serviceName: booking['serviceName'],
+                            date: booking['date'],
+                            status: booking['status'],
+                            email: booking['email'],
+                            phone: booking['phone'],
+                            price: booking['price'],
+                          );
+                        });
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
             )
           ],
         ),
@@ -114,8 +117,6 @@ class CustomerHomeView extends StackedView<CustomerHomeViewModel> {
   }
 
   @override
-  CustomerHomeViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
+  CustomerHomeViewModel viewModelBuilder(BuildContext context) =>
       CustomerHomeViewModel();
 }
